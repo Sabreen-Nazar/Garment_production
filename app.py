aa770dcb2c8c9bdec983e70aa5dde04f3c5fe7ce
Sabreen-Nazar <nazarsabreen@gmail.com>
2023-07-08 13:25:19 +0530

    flask app addes

--- app.py
+++ app.py
@@ -6,32 +6,15 @@ import numpy as np
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
-#print("all imported")
 
 app = Flask(__name__)
-#print("app created")
+
 
 # Load the model
 
 model_gb = pickle.load(open("gradient_model.pkl","rb"))
 scalar = pickle.load(open('scaling.pkl' , 'rb'))
-#print("model created")
 
-
-# dat = {
-# 		"quarter": 0.00,
-# 		"department": 1.00,
-# 		"team": 9.00,
-# 		"targeted_productivity": 0.70,
-# 		"smv": 28.08,
-# 		"wip": 872.00,
-# 		"over_time": 6900.00,
-# 		"incentive": 44.00,
-# 		"idle_time": 0.00,
-# 		"idle_men": 0.00,
-# 		"no_of_style_change": 0.00,
-# 		"no_of_workers": 57.50
-# 	}
 
 
 
@@ -55,26 +38,11 @@ def predict():
     #final_input=scalar.transform(np.array(data).reshape(1,-1))
     final_input=np.array(data).reshape(1,-1)
     print(final_input)
-    output=model_gb.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The Garment production is :  {}".format(output))
+    output=round(model_gb.predict(final_input)[0]*100)
+    return render_template("home.html",prediction_text="The Garment production percentage is :  {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
 
-# {
-# 	"data": {
-# 		"quarter": 0.00,
-# 		"department": 1.00,
-# 		"team": 9.00,
-# 		"targeted_productivity": 0.70,
-# 		"smv": 28.08,
-# 		"wip": 872.00,
-# 		"over_time": 6900.00,
-# 		"incentive": 44.00,
-# 		"idle_time": 0.00,
-# 		"idle_men": 0.00,
-# 		"no_of_style_change": 0.00,
-# 		"no_of_workers": 57.50
-# 	}
-# }
 
+# <input type="text" name="department" placeholder="Department" required="required" /><br>
